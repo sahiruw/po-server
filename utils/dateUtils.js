49ad52ed8d305d194ee80eac8ec05a2e301f56b3
sahiru <sahiruwrcg@gmail.com>
2023-11-01 03:15:01 +0530

    changed date to gmt + 5.30

--- utils/dateUtils.js
+++ utils/dateUtils.js
@@ -1,5 +1,11 @@
 const getToday = () => {
+  // Get the current date in GMT
   const currentDate = new Date();
+  
+  // Add 5 hours and 30 minutes to adjust for GMT+5.30
+  currentDate.setHours(currentDate.getHours() + 5);
+  currentDate.setMinutes(currentDate.getMinutes() + 30);
+
   const day = currentDate.getDate().toString().padStart(2, "0"); // Ensure 2-digit day
   const month = (currentDate.getMonth() + 1).toString().padStart(2, "0"); // Ensure 2-digit month
   const year = currentDate.getFullYear().toString(); // Full year
